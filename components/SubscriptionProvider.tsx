b6585b0f812d694f3875b01b8fc4e826222126ad
Djennkins <djennkins228@gmail.com>
2024-11-14 18:34:22 +0200

    implemented Zustand for State Manager

--- components/SubscriptionProvider.tsx
+++ components/SubscriptionProvider.tsx
@@ -1,29 +1,37 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { subscriptionRef } from '@/lib/converters/Subscription';
 import { onSnapshot } from '@firebase/firestore';
+import { useSubscriptionStore } from '@/store/store';
 
-function SubscriptionProvider() {
+function SubscriptionProvider({ children }: { children: ReactNode }) {
     const { data: session } = useSession();
+    const setSubscription = useSubscriptionStore((state) => state.setSubscription);
 
     useEffect(() => {
         if (!session) return;
 
-        return onSnapshot(subscriptionRef(session?.user.id), (snapshot) => {
-            if (snapshot.empty) {
-                console.log('User has NO subscription.');
-                return;
-            } else {
-                console.log('User has subscription.');
-
-                //set Subscription
+        return onSnapshot(
+            subscriptionRef(session?.user.id),
+            (snapshot) => {
+                if (snapshot.empty) {
+                    console.log('User has NO subscription.');
+                    setSubscription(null);
+                    return;
+                } else {
+                    console.log('User has subscription.');
+                    setSubscription(snapshot.docs[0].data());
+                }
+            },
+            (error) => {
+                console.error('error');
             }
-        });
-    }, [session]);
+        );
+    }, [session, setSubscription]);
 
-    return <div>Subscription Provider</div>;
+    return <div>{children}</div>;
 }
 
 export default SubscriptionProvider;
