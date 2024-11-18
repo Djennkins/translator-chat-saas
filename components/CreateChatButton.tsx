1904a5c36fe32142c01fc16fe30aaf2e04c25058
Djennkins <djennkins228@gmail.com>
2024-11-18 14:51:08 +0200

    implemented functionality for adding a chat

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -9,6 +9,9 @@ import { useSubscriptionStore } from '@/store/store';
 import LoadingSpinner from '@/components/LoadingSpinner';
 import { useToast } from '@/hooks/use-toast';
 import { v4 as uuidv4 } from 'uuid';
+import { addChatRef } from '@/lib/converters/ChatMembers';
+import { serverTimestamp } from '@firebase/database';
+import { setDoc } from '@firebase/firestore';
 
 function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
     const router = useRouter();
@@ -33,7 +36,33 @@ function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
 
         const chatId = uuidv4();
 
-        router.push('/chat/abc');
+        await setDoc(addChatRef(chatId, session.user.id), {
+            userId: session.user.id!,
+            email: session.user.email!,
+            timestamp: serverTimestamp(),
+            isAdmin: true,
+            chatId: chatId,
+            image: session.user.image || '',
+        })
+            .then(() => {
+                toast({
+                    title: 'Success',
+                    description: 'Your chat has been created!',
+                    className: 'bg-green-600 text-white',
+                    duration: 2000,
+                });
+                router.push(`/chat/${chatId}`);
+            })
+            .catch((err) => {
+                toast({
+                    title: 'Error',
+                    description: 'There was an error creating your chat!',
+                    variant: 'destructive',
+                });
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     };
 
     if (isLarge) {
