1cb2bbabce4b84b7269837b662b3def974bd739f
Djennkins <djennkins228@gmail.com>
2024-11-18 10:27:27 +0200

    built the template of chat pages

---
+++ app/(user)/chat/[chatId]/page.tsx
@@ -0,0 +1,12 @@
+function ChatPage() {
+    return (
+        <>
+            {/*    Admin controls*/}
+            {/*    ChatMembers Badge*/}
+            {/*    Chat Messages*/}
+            {/*    Chat Input*/}
+        </>
+    );
+}
+
+export default ChatPage;
