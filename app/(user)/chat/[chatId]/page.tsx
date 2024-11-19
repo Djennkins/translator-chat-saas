ffc6faa99f04de72fee5bc9a26b28d3e54b927df
Djennkins <djennkins228@gmail.com>
2024-11-19 21:10:13 +0200

    built the Chat Messages Component

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,6 +1,9 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/auth';
 import ChatInput from '@/components/ChatInput';
+import { sortedMessagesRef } from '@/lib/converters/Message';
+import { getDocs } from '@firebase/firestore';
+import ChatMessages from '@/components/ChatMessages';
 
 type Props = {
     params: {
@@ -11,14 +14,18 @@ type Props = {
 async function ChatPage({ params: { chatId } }: Props) {
     const session = await getServerSession(authOptions);
 
+    const initialMessages = (await getDocs(sortedMessagesRef(chatId))).docs.map((doc) =>
+        doc.data()
+    );
+
     return (
-        <>
-            {/*    Admin controls*/}
-            {/*    ChatMembers Badge*/}
-            {/*    Chat Messages*/}
-            {/*Chat Input*/}
+        <div>
+            <div className="flex-1">
+                <ChatMessages chatId={chatId} session={session} initialMessages={initialMessages} />
+            </div>
+
             <ChatInput chatId={chatId} />
-        </>
+        </div>
     );
 }
 
