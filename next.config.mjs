29d9a53e43e2dafe7e91d2f1c9d105037c54dee4
Djennkins <djennkins228@gmail.com>
2024-11-21 16:10:17 +0200

    updated rule for ignoring eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['github.com', 'lh3.googleusercontent.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
