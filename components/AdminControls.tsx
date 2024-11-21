357126e5ad9b9b5fc781ad6ccc1c95e2015c0cc8
Djennkins <djennkins228@gmail.com>
2024-11-21 13:48:20 +0200

    built the delete chat functionality

--- components/AdminControls.tsx
+++ components/AdminControls.tsx
@@ -1,10 +1,11 @@
 import InviteUser from '@/components/InviteUser';
+import DeleteChatButton from '@/components/DeleteChatButton';
 
 function AdminControls({ chatId }: { chatId: string }): JSX.Element {
     return (
         <div className="flex justify-end space-x-2 m-5 mb-0">
             <InviteUser chatId={chatId} />
-            {/*<DeleteChatButton chatId={chatId}></DeleteChatButton>*/}
+            <DeleteChatButton chatId={chatId} />
         </div>
     );
 }
