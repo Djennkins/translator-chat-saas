9a06cd176fcf21ae25dc6885205e52b8fbce2851
Djennkins <djennkins228@gmail.com>
2024-11-22 00:17:37 +0200

    removed svg, fixed p, and changed svg on main page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -12,8 +12,7 @@ async function PricingPage() {
                 <div className="mx-auto max-w-4xl">
                     <h2 className="text-base font-semibold leading-7 text-indigo-400">Pricing</h2>
                     <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
-                        The right price for you,{''}
-                        <br className="hidden sm:inline lg:hidden" />
+                        The right price for you, <br className="hidden sm:inline lg:hidden" />
                         whoever you are
                     </p>
                 </div>
